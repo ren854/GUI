cf2296e33e519c4b546be99af6e06413d5ce21be
LoloM <ily19932014@gmail.com>
2020-11-16 11:54:21 +0800

    稍微修改一點點

--- 抓商品名稱加價格.py
+++ 抓商品名稱加價格.py
@@ -14,22 +14,34 @@ import wget
 
 headers = {'cookie': 'ECC=GoogleBot',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
-           }   
-url ="https://www.lativ.com.tw/Product/GetNewProductCategoryList?MainCategory=WOMEN&pageIndex=0&cacheID=35976"
-res = requests.get(url,headers=headers)
-data = json.loads(res.text)
+           }
+s = str(input("想找什麼款式呢? "))
 
-for i in range(len(data)):
-    print(str(data[i]['ProductName'])+str(data[i]['Price']))
+aa=[]
+for p in range(5):   
+    url ="https://www.lativ.com.tw/Search/DataList?keyword={}&ec=0&t=0&page={}&category=ALL&cacheID=36182".format(s,p)
+    res = requests.get(url,headers=headers)
+    data = json.loads(res.text)
+
+    for i in range(len(data)):
+        print(str(data[i]['Name'])+str(data[i]['Price']))
+        aa.append(data)
+        
 
     
-import csv
 
 
-with open('price.csv', 'w', newline='',encoding='utf-8-sig') as csvfile:
-  
-  writer = csv.writer(csvfile,delimiter=' ')
 
-  
-  for i in range(len(data)) :
-     writer.writerow(str(data[i]['ProductName'])+str(data[i]['Price']))
+with open('a.csv', 'a' , encoding = 'utf-8-sig' , newline = '') as csvfile:
+    w = csv.writer(csvfile)
+    filednames=(['名稱','價格','圖1'])
+    w = csv.DictWriter(csvfile,fieldnames=filednames)
+    w.writeheader()
+    sleep(0.7)                    
+    for i in range(len(aa)):
+        for j in range(len(aa[i])):
+            w.writerow({'名稱':aa[i][j]['Name'],
+                        '價格':aa[i][j]['Price'],
+                        '圖1':"https://s2.lativ.com.tw" + aa[i][j]['Image']})
+
+
